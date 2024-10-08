9f81bd4632ed515e7ffc686e61338d76c356b374
Shubham Saklani <saklanishubham15@gmail.com>
2024-10-08 23:36:02 +0530

    chore: added constants for apiUrl

--- src/app/services/profile-service.ts
+++ src/app/services/profile-service.ts
@@ -1,21 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { constants } from '../utility/constants';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
+  apiUrl = constants.apiUrl;
+
   constructor(private http: HttpClient) { }
 
   //update profile image of user
   updateProfileImage(formData: FormData): Observable<any> {
-    return this.http.post<any>("http://localhost:3000/update-profile-image/", formData);
+    return this.http.post<any>(`${this.apiUrl}/update-profile-image/`, formData);
   }
 
   //get profile image of user
   getProfileImage(username: string | null): Observable<any> {
-    return this.http.get<any>("http://localhost:3000/get-profile-image/"+username, { responseType: 'blob' as 'json' });
+    return this.http.get<any>(`${this.apiUrl}/get-profile-image/` + username, { responseType: 'blob' as 'json' });
   }
 }
